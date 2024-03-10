174a76db7c20509922c382c4a75d451eee2a6622
diyantonchev <DiyanTonchev@users.noreply.github.com>
2024-03-10 22:56:33 +0200

    Fix failed deplpoy

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { ChevronRight, MoreHorizontal } from 'lucide-react';
