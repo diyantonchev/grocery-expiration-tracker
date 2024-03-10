653988d996d08ffe5c75b0726b99303144669e63
diyantonchev <DiyanTonchev@users.noreply.github.com>
2024-03-10 22:38:52 +0200

    Added breadcrumbs

--- src/app/groceries/[id]/page.tsx
+++ src/app/groceries/[id]/page.tsx
@@ -3,6 +3,15 @@ import Image from 'next/image';
 import dayjs from 'dayjs';
 
 import { getGrocery } from '~/server/data/groceries';
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '~/components/ui/breadcrumb';
+
 import { CardBody, CardContainer, CardItem } from '~/components/ui/3d-card';
 import { Button } from '~/components/ui/button';
 import { cn } from '~/lib/utils';
@@ -29,53 +38,68 @@ export default async function GroceryPage({ params }: GroceryPageProps) {
   }
 
   const isExpired = dayjs(grocery.expirationDate).isBefore(dayjs());
+  const title = `${grocery.productName} ${grocery.brand && grocery.brand}`;
 
   return (
-    <CardContainer className="inter-var">
-      <CardBody className="group/card relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]">
-        <CardItem
-          translateZ="50"
-          className="text-2xl font-bold text-neutral-600 dark:text-white"
-        >
-          {grocery.productName} {grocery.brand && `${grocery.brand}`}
-        </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="mt-2 max-w-sm text-neutral-500 dark:text-neutral-300"
-        >
-          {grocery.quantity} {grocery.unit && `${grocery.unit}`}
-        </CardItem>
-        <CardItem
-          as="p"
-          translateZ="60"
-          className="mt-2 max-w-sm text-neutral-500 dark:text-neutral-300"
-        >
-          Best before{' '}
-          <span className={cn(isExpired ? 'text-red-500' : '')}>
-            {dayjs(grocery.expirationDate).format('DD.MM.YY')}
-          </span>
-        </CardItem>
-        <CardItem translateZ="100" className="mt-4 w-full">
-          <Image
-            src="/placeholder.jpg"
-            height="1000"
-            width="1000"
-            className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
-            alt="thumbnail"
-          />
-        </CardItem>
-        <div className="mt-8 flex items-center justify-end gap-2">
-          <CardItem translateZ={20}>
-            <Button className="bg-green-500 px-6 hover:bg-green-500/90 dark:bg-green-900 dark:text-gray-50 dark:hover:bg-green-900/90">
-              Edit
-            </Button>
+    <>
+      <Breadcrumb className="m-2 md:m-4">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/groceries">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>{title}</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <CardContainer className="inter-var">
+        <CardBody className="group/card relative h-auto w-auto rounded-xl border border-black/[0.1] bg-gray-50 p-6 dark:border-white/[0.2] dark:bg-black dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] sm:w-[30rem]">
+          <CardItem
+            translateZ="50"
+            className="text-2xl font-bold text-neutral-600 dark:text-white"
+          >
+            {grocery.productName} {grocery.brand && `${grocery.brand}`}
           </CardItem>
-          <CardItem translateZ={20}>
-            <GroceryDeleteButton groceryId={grocery.id} />
+          <CardItem
+            as="p"
+            translateZ="60"
+            className="mt-2 max-w-sm text-neutral-500 dark:text-neutral-300"
+          >
+            {title}
           </CardItem>
-        </div>
-      </CardBody>
-    </CardContainer>
+          <CardItem
+            as="p"
+            translateZ="60"
+            className="mt-2 max-w-sm text-neutral-500 dark:text-neutral-300"
+          >
+            Best before{' '}
+            <span className={cn(isExpired ? 'text-red-500' : '')}>
+              {dayjs(grocery.expirationDate).format('DD.MM.YY')}
+            </span>
+          </CardItem>
+          <CardItem translateZ="100" className="mt-4 w-full">
+            <Image
+              src="/placeholder.jpg"
+              height="1000"
+              width="1000"
+              className="h-60 w-full rounded-xl object-cover group-hover/card:shadow-xl"
+              alt="thumbnail"
+            />
+          </CardItem>
+          <div className="mt-8 flex items-center justify-end gap-2">
+            <CardItem translateZ={20}>
+              <Button className="bg-green-500 px-6 hover:bg-green-500/90 dark:bg-green-900 dark:text-gray-50 dark:hover:bg-green-900/90">
+                Edit
+              </Button>
+            </CardItem>
+            <CardItem translateZ={20}>
+              <GroceryDeleteButton groceryId={grocery.id} />
+            </CardItem>
+          </div>
+        </CardBody>
+      </CardContainer>
+    </>
   );
 }
--- src/app/groceries/_components/header.tsx
+++ src/app/groceries/_components/header.tsx
@@ -11,10 +11,6 @@ import BurgerMenu from './burger-menu';
 export default function Header() {
   return (
     <header className="flex h-14 items-center gap-4 border-b bg-gray-100/40 px-4 dark:bg-gray-800/40">
-      <Link className="hidden md:block" href="/groceries">
-        <CounterClockwiseClockIcon className="h-6 w-6" />
-        <span className="sr-only">Home</span>
-      </Link>
       <BurgerMenu />
       <div className="w-full flex-1">
         <form>
